3e4c2f27b482cb84177a628a025ce5e7ca3c32cb
reidzeibel <reidzeibel@gmail.com>
2016-01-10 21:30:32 +0700

    Initial Android Commit

---
+++ teladan48app/src/main/java/com/reidzeibel/teladan48app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.reidzeibel.teladan48app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
