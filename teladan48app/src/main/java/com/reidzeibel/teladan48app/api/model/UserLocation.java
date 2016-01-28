52e639998d265a040b40321aeb749356ec4931b6
reidzeibel <reidzeibel@gmail.com>
2016-01-28 23:29:45 +0700

    Fix UserLocation Model to match the response from backend
    - Remove extends user, added User model-related methods

--- teladan48app/src/main/java/com/reidzeibel/teladan48app/api/model/UserLocation.java
+++ teladan48app/src/main/java/com/reidzeibel/teladan48app/api/model/UserLocation.java
@@ -3,10 +3,64 @@ package com.reidzeibel.teladan48app.api.model;
 /**
  * Created by Reidzeibel on 12/01/2016.
  */
-public class UserLocation extends User {
+public class UserLocation {
 
+    private long id;
+    private String name;
+    private String email;
+    private String created_at;
+    private String updated_at;
     private String location_lat;
     private String location_long;
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
 
     public String getLocation_lat() {
         return location_lat;
@@ -32,6 +86,7 @@ public class UserLocation extends User {
                 + "location_lat : " + getLocation_lat() + "\r\n"
                 + "location_long : " + getLocation_long() + "\r\n"
                 + "created_at : " + getCreated_at() + "\r\n"
-                + "updated_at : " + getUpdated_at() + "\r\n\r\n";
+                + "updated_at : " + getUpdated_at() + "\r\n"
+                + "user : " + getUser() + "\r\n";
     }
 }
